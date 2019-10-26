46f8ad993a05ff07e45a66788ae6c560d7c71163
John Watson <johnwatson484@hotmail.com>
2019-10-26 11:53:46 +0100

    Logging and errors added

--- server/index.js
+++ server/index.js
@@ -1,15 +1,18 @@
 const hapi = require('@hapi/hapi')
+const config = require('./config')
 
 async function createServer () {
   // Create the hapi server
   const server = hapi.server({
-    port: 3000
+    port: config.port
   })
 
   // Register the plugins
   await server.register(require('@hapi/inert'))
   await server.register(require('./plugins/views'))
   await server.register(require('./plugins/router'))
+  await server.register(require('./plugins/errors'))
+  await server.register(require('./plugins/logging'))
   await server.register(require('blipp'))
 
   return server
